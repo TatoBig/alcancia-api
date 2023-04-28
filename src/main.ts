b888cdff52dd4109581d07a7a2c36228d06995eb
tatobig <tatobig@gmail.com>
2023-04-28 08:49:58 +0000

    Merge

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,6 @@ async function bootstrap() {
   SwaggerModule.setup('doc', app, document);
 
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
